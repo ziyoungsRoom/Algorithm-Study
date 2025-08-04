17a8d64b6e257765f97af88de2780f0988d3b450
Minae <ydmaad@gmail.com>
2025-08-04 14:57:53 +0900

    feat: 1330번_두 수 비교하기

--- Minae/BaekJoon/Step2_조건문/1330_두수비교하기.js
+++ Minae/BaekJoon/Step2_조건문/1330_두수비교하기.js
@@ -1,1 +1,9 @@
-테스트입니답;
+// 언어 : Javascript , (성공/실패) : 3/1 , 메모리 : 9332 KB , 시간 : 92 ms
+const input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
+
+const A = parseInt(input[0]);
+const B = parseInt(input[1]);
+
+if (A > B) console.log(">");
+if (A < B) console.log("<");
+if (A === B) console.log("==");
