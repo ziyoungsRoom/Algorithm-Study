3d22b99043518a2cafe349ee7c15d6d0dc70e219
Minae <ydmaad@gmail.com>
2025-08-05 14:49:04 +0900

    feat: 구구단 문제 풀이 완료

---
+++ Minae/BaekJoon/Class1/2739_구구단.js
@@ -0,0 +1,8 @@
+// 언어 : Javascript , (성공/실패) : 1/1 , 메모리 : 9352 KB , 시간 : 140 ms
+const input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
+
+const N = Number(input);
+
+for (let i = 1; i <= 9; i++) {
+  console.log(`${N} * ${i} = ${N * i}`);
+}
