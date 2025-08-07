fa72743b403dedd1e2b32408b806da45963accd5
Minae <ydmaad@gmail.com>
2025-08-07 15:10:14 +0900

    feat: 새싹 문제 풀이 완료

---
+++ Minae/BaekJoon/Class1/25083_새싹.js
@@ -0,0 +1,7 @@
+// 언어 : Javascript , (성공/실패) : 2/4 , 메모리 : 9284 KB , 시간 : 88 ms
+console.log("         ,r'\"7");
+console.log("r`-_   ,'  ,/");
+console.log(" \\. \". L_r'");
+console.log("   `~\\/");
+console.log("      |");
+console.log("      |");
