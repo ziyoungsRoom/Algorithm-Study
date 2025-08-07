8a1eb254a3772684a8dbbe4b85ffbbc1638fa013
Minae <ydmaad@gmail.com>
2025-08-07 15:27:16 +0900

    fix: 주석 수정

--- Minae/BaekJoon/Class1/27866_문자와문자열.js
+++ Minae/BaekJoon/Class1/27866_문자와문자열.js
@@ -1,4 +1,4 @@
-// 언어 : Javascript , (성공/실패) : 2/4 , 메모리 : 9284 KB , 시간 : 88 ms
+// 언어 : Javascript , (성공/실패) : 1/5 , 메모리 : 9322 KB , 시간 : 96 ms
 const input = require("fs").readFileSync("/dev/stdin").toString().split("\n");
 
 const S = input[0];
