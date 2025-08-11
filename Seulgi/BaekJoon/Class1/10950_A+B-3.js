c4b0bb378d9792e5de914d66575bded5e8f33a50
Seul-Ki Choi <hanbat107@naver.com>
2025-08-11 10:36:40 +0900

    feat: 10950 A+B-3 문제 풀이

---
+++ Seulgi/BaekJoon/Class1/10950_A+B-3.js
@@ -0,0 +1,10 @@
+// 문제 : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const T = Number(input[0]);
+
+for(let i = 1; i <= T; i++){
+    const [a, b] = input[i].split(' ').map(Number);
+    console.log(a + b);
+}
