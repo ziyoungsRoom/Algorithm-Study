a2413ee1b2e692f335569bae501fb48fcdc53dc6
Seul-Ki Choi <hanbat107@naver.com>
2025-08-11 10:34:20 +0900

    feat: 10869 사칙연산 문제 풀이

---
+++ Seulgi/BaekJoon/Class1/10869_사칙연산.js
@@ -0,0 +1,11 @@
+// 문제 : 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+const a = Number(input[0]);
+const b = Number(input[1]);
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(Math.floor(a / b));
+console.log(a % b);
