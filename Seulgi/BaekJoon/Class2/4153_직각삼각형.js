0299f71013f79c653d3dd668eb5c3bdc6ff344ca
Seul-Ki Choi <hanbat107@naver.com>
2025-08-11 22:54:17 +0900

    feat: 4153 직각삼각형 문제 풀이

---
+++ Seulgi/BaekJoon/Class2/4153_직각삼각형.js
@@ -0,0 +1,17 @@
+// 문제 : 과거 이집트인들은 각 변들의 길이가 3, 4, 5인 삼각형이 직각 삼각형인것을 알아냈다. 주어진 세변의 길이로 삼각형이 직각인지 아닌지 구분하시오.
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+for(let line of input){
+    const [a, b, c] = line.split(' ').map(Number);
+    if(a === 0 && b === 0 && c === 0) break;
+    
+    const maxSide = Math.max(a, b, c);
+    const sides = [a, b, c].filter(side => side !== maxSide);
+    
+    if(sides[0]**2 + sides[1]**2 === maxSide**2){
+        console.log("right");
+    } else {
+        console.log("wrong");
+    }
+}
